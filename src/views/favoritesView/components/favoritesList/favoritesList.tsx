c714e36a399f3ff93e05a3f780fb1c2fd3226c94
Shay Bushary <shaybush93@gmail.com>
2024-01-06 20:32:10 +0200

    empty favorite

--- src/views/favoritesView/components/favoritesList/favoritesList.tsx
+++ src/views/favoritesView/components/favoritesList/favoritesList.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from "../../../../redux/hooks";
+import FavoritesNotFound from "../../../../shared/components/favoritesNotFound";
 import FavoriteItem from "../favoriteItem";
 
 const FavoritesList = () => {
@@ -10,11 +11,19 @@ const FavoritesList = () => {
     <div className="p-2">
       <h2 className="mb-4">Favorite List:</h2>
       <div className="row">
-        {favoritesArray.map((favorite) => (
-          <div key={favorite.key} className="col-lg-3 col-md-4 col-6 p-2">
-            <FavoriteItem favorite={favorite} />
-          </div>
-        ))}
+        {favoritesArray.length ? (
+          favoritesArray.map((favorite) => (
+            <div key={favorite.key} className="col-lg-3 col-md-4 col-6 p-2">
+              <FavoriteItem favorite={favorite} />
+            </div>
+          ))
+        ) : (
+          <FavoritesNotFound
+            styleClass="text-center text-mute"
+            width={"70%"}
+            height="300"
+          />
+        )}
       </div>
     </div>
   );
